4c09ca2b98e67fcdf2bfb2f90301c2fcb99419cd
MustafaAswadi <aswadimustafa16@gmail.com>
2023-11-29 21:31:05 +0430

    setup axios and create a layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import MealList from "../views/MealsByName.vue";
+import MealsByName from "../views/MealsByName.vue";
+import MealsByLetter from "../views/MealsByLetter.vue";
+import MealsByIngredient from "../views/MealsByIngredient.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/by-name/:name?",
+    name: "byName",
+    component: MealsByName,
+  },
+  {
+    path: "/by-letter/:letter?",
+    name: "byLetter",
+    component: MealsByLetter,
+  },
+  {
+    path: "/by-ingredient/:ingredient?",
+    name: "byIngredient",
+    component: MealsByIngredient,
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="flex p-10 justify-center">
-    <input type="text" class="rounded-2xl border-2 border-gray-400" />
-    {{ meals }}
+  <div class="flex flex-col p-8">
+    <div>
+      <input
+        type="text"
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="Search for Meals"
+      />
+    </div>
+    <div class="flex gap-2 mt-2 justify-center">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import store from "../store";
+import { onMounted } from "vue";
+import axiosClient from "../axiosClient";
 
-const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+onMounted(async () => {
+  const res = await axiosClient.get("/list.php?i=list");
+  console.log(res.data);
+});
 </script>
-
-<style scoped></style>
