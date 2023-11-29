326d888a40ca4c88583da13daefa1be21f95a0fb
MustafaAswadi <aswadimustafa16@gmail.com>
2023-11-29 20:03:58 +0430

    configure tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>{{ meals }}</div>
+  <div class="flex p-10 justify-center">
+    <input type="text" class="rounded-2xl border-2 border-gray-400" />
+    {{ meals }}
+  </div>
 </template>
 
 <script setup lang="ts">
